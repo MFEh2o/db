65d8068ff09ec3f70b1e1fe3da8d29409429e9c2
joneslabND <sjones20@nd.edu>
2020-05-12 09:33:59 -0400

    modified dbdir and dbname arguments so both manual and sensor databases can be used in same script

--- sensordbTable.R
+++ sensordbTable.R
@@ -104,8 +104,8 @@ sensordbTable<-function(table,fpath=dbdir,
 # fpath - file path of database file location; currently defaults to Jake's computer location
 # dbname - name of database, default is 'MFEdb_20170202.db' ; maybe change to current version so we don't have to continue to update this function
 
-sensordbTableList<-function(fpath=dbdir,
-                      dbname=db){
+sensordbTableList<-function(fpath=sensor_dbdir,
+                      sensor_dbname=db){
   #set file path to the location of the database (defaults to Jake's database location)
   library(RSQLite)
   drv=SQLite() #create driver object
