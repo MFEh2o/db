b5b70ac4b418450b6b0febbe91954219c1595e61
joneslabND <sjones20@nd.edu>
2020-05-12 09:36:05 -0400

    fixed one error with sensor_dbname

--- sensordbTable.R
+++ sensordbTable.R
@@ -105,7 +105,7 @@ sensordbTable<-function(table,fpath=dbdir,
 # dbname - name of database, default is 'MFEdb_20170202.db' ; maybe change to current version so we don't have to continue to update this function
 
 sensordbTableList<-function(fpath=sensor_dbdir,
-                      sensor_dbname=db){
+                      dbname=sensor_db){
   #set file path to the location of the database (defaults to Jake's database location)
   library(RSQLite)
   drv=SQLite() #create driver object
